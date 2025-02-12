8c1017533c38beea7df7256ff56fdc630303ecf2
Gabriel Bertolini <gabrielbertolini2002@hotmail.om>
2025-02-12 09:44:34 -0300

    new exercises

--- exercises/15_height_while.py
+++ exercises/15_height_while.py
@@ -1,10 +1,10 @@
 sum=0
-quantity_entries=4 #counter of entries
+entries_quantity=4 #counter of entries
 
-while quantity_entries >0:
+while entries_quantity >0:
     height =input("Enter the height: ")
     height=float(height)
     sum += height
-    quantity_entries -=1
+    entries_quantity -=1
 
 print("Height sum: ",sum)
--- exercises/17_height_for.py
+++ exercises/17_height_for.py
@@ -1,9 +1,9 @@
 # %%
 
 sum = 0
-quantity_entries = 4  # Counter of entries
+entries_quantity = 4  # Counter of entries
 
-for i in range(quantity_entries):  # Loop runs 4 times
+for i in range(entries_quantity):  # Loop runs 4 times
     height = input("Enter the height: ")  # Indented properly inside loop
     height = float(height)  # Convert input to float
     sum += height  # Add to sum
