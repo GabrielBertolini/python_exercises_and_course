293c719a5b34d1796ede46d4275d11c448f551df
Gabriel Bertolini <gabrielbertolini2002@hotmail.om>
2025-02-12 09:41:50 -0300

    new exercise

--- exercises/17_height_for.py
+++ exercises/17_height_for.py
@@ -1,12 +1,11 @@
 # %%
 
-sum=0
-quantity_entries=4 #counter of entries
+sum = 0
+quantity_entries = 4  # Counter of entries
 
-for i in range(quantity_entries)
-height =input("Enter the height: ")
-height=float(height)
-sum += height
-quantity_entries -=1
+for i in range(quantity_entries):  # Loop runs 4 times
+    height = input("Enter the height: ")  # Indented properly inside loop
+    height = float(height)  # Convert input to float
+    sum += height  # Add to sum
 
-print("Height sum: ",sum)
+print("Height sum:", sum)  # Print final sum
